d334f46be25ac8923a0008404a539b0a55d4b372
Joshua Isaacs <jisaacs1207@gmail.com>
2014-06-22 19:23:03 +0800

    Contribute command inclusion.

--- src/main/java/io/github/jisaacs1207/contribute/Contribute.java
+++ src/main/java/io/github/jisaacs1207/contribute/Contribute.java
@@ -1,24 +1,36 @@
 package io.github.jisaacs1207.contribute;
 
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
 
 public final class Contribute extends JavaPlugin{
+	
+	public String prefix = "[CTB]";
+	public String onLoad = " has been successfully loaded.";
+	public String onUnload = " has been successfully unloaded.";
+	
+	
+	
 	@Override
 	public void onEnable() {
-		getLogger().info("Contribute has been successfully loaded");
+		getLogger().info(this.onLoad);
 	}
  
 	@Override
 	public void onDisable() {
-		getLogger().info("Contribute has been successfully unloaded");
+		getLogger().info(this.onUnload);
 	}
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (cmd.getName().equalsIgnoreCase("contribute")) {
-			// doSomething
+		if (cmd.getName().equalsIgnoreCase("contribute")) { // If the player typed /basic then do the following...
+			Player donatorPlayer = (Player) sender;
+			sender.sendMessage(ChatColor.GOLD + this.prefix + ChatColor.GREEN + " Hotdogs!");
 			return true;
 		} //If this has happened the function will return true. 
 	        // If this hasn't happened the value of false will be returned.
