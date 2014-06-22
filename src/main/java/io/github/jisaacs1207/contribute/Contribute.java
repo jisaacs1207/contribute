d68938915c4e41ef3506318a27334d8104963267
Joshua Isaacs <jisaacs1207@gmail.com>
2014-06-23 01:44:55 +0800

    Added a config file and began learning how to enable the add commands.

--- src/main/java/io/github/jisaacs1207/contribute/Contribute.java
+++ src/main/java/io/github/jisaacs1207/contribute/Contribute.java
@@ -21,11 +21,51 @@ public final class Contribute extends JavaPlugin{
 	public String onUnload = " has been successfully unloaded.";
 	public String syntaxError = "You are clearly confuzzled. /contribute help";
 	
+	// Contribution Item Names
+	
+	public String dItemNm1;
+	public String dItemNm2;
+	public String dItemNm3;
+	public String dItemNm4;
+	
+	// Contribution Item Ids
+	
+	public String dItemId1;
+	public String dItemId2;
+	public String dItemId3;
+	public String dItemId4;
+	
+	// Kingdom Names
+	
+	public String kingdom1;
+	public String kingdom2;
+	public String kingdom3;
+	public String kingdom4;
+	public String kingdom5;
+	
 	
 	
 	@Override
 	public void onEnable() {
 		getLogger().info(this.onLoad);
+		
+		// Set the default config file if it isn't there.
+		saveDefaultConfig();
+		
+		// Pull goodies from the config file
+		dItemNm1 = getConfig().getString("FirstDonationItemName");
+		dItemNm2 = getConfig().getString("SecondDonationItemName");
+		dItemNm3 = getConfig().getString("ThirdDonationItemName");
+		dItemNm4 = getConfig().getString("FourthDonationItemName");
+		dItemId1 = getConfig().getString("FirstDonationItemId");
+		dItemId2 = getConfig().getString("SecondDonationItemId");
+		dItemId3 = getConfig().getString("ThirdDonationItemId");
+		dItemId4 = getConfig().getString("FourthDonationItemId");
+		kingdom1 = getConfig().getString("FirstKingdomName");
+		kingdom2 = getConfig().getString("SecondKingdomName");
+		kingdom3 = getConfig().getString("ThirdKingdomName");
+		kingdom4 = getConfig().getString("FourthKingdomName");
+		kingdom5 = getConfig().getString("FifthKingdomName");
 	}
  
 	@Override
@@ -57,12 +97,25 @@ public final class Contribute extends JavaPlugin{
 					}
 				}
 				else if (args[0].equals("info")) {
-					sender.sendMessage("     -= Five Kingdoms Contribute Info =-");
+					sender.sendMessage("     -= Five Kingdoms Contribute Winners =-");
+					sender.sendMessage("");
+					// kingdom here is a temp stub!! This needs to be changed to decide the winning kingdom later
+					sender.sendMessage(dItemNm1 + " - " + kingdom1);
+					sender.sendMessage(dItemNm2 + " - " + kingdom2);
+					sender.sendMessage(dItemNm3 + " - " + kingdom3);
+					sender.sendMessage(dItemNm4 + " - " + kingdom4);
 					}
 				}
 			else if (args.length == 2) {
 				if (args[0].equals("add")) {
-					//add stub
+					if ((!args[1].equals(dItemNm1))&&(!args[1].equals(dItemNm2))&&(!args[1].equals(dItemNm3))&&(!args[1].equals(dItemNm4))) {
+						sender.sendMessage("You can only donate the following items:");
+						sender.sendMessage(dItemNm1);
+						sender.sendMessage(dItemNm2);
+						sender.sendMessage(dItemNm3);
+						sender.sendMessage(dItemNm4);
+					}
+					
 				}
 				else if (args[0].equals("info")) {
 					//info stub
